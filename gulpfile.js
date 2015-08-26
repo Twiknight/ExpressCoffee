365ffe15fb89cd9cf2ffb7c1ff6cfd9755bf2c35
Twiknight <twiknight@163.com>
2015-08-26 22:49:44 +0800

    add init.coffee
    created init.coffee

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
+var paths = ['CoffeeSource/**/*.coffee'];
 
 gulp.task('compile', function () {
-  gulp.src('CoffeeSource/*.coffee')
+  gulp.src(paths)
       .pipe(coffee())
       .pipe(gulp.dest("CompiledJs"));
 });
 
 gulp.task('watch',function () {
-  gulp.watch('CoffeeSource/*.coffee',['compile']);
+  gulp.watch(paths,['compile']);
 });
 
 gulp.task('default',['watch','compile']);
