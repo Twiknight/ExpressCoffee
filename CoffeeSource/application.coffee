7ec5f36813b7e82687cfda2476c8d68fed72da3b
Twiknight <twiknight@163.com>
2015-08-30 15:21:09 +0800

    add application.coffee

---
+++ CoffeeSource/application.coffee
@@ -0,0 +1,278 @@
+finalhandler = require 'finalhandler'
+Router = require './router'
+methods = require 'methods'
+middleware = require './middleware/init'
+query = require './middleware/query'
+debug = require('debug') 'express: application'
+View = require './view'
+http = require 'http'
+compileETag = require('./utils').compileETag
+compileQueryParser = require('./utils').compileQueryParser
+compileTrust = require('./utils').compileTrust
+deprecate = require('depd') 'express'
+flatten = require 'array-flatten'
+merge = require 'utils-merge'
+resolve = require('path').resolve
+slice = Array.prototype.slice
+
+app = exports = module.exports ={}
+
+trustProxyDefaultSymbol = '@@symbol:trust_proxy_default'
+
+logerror = (err) ->
+  if @get('env') != 'test'
+    console.error err.stack || err.toString()
+  return
+
+tryRender = (view, options, callback) ->
+  try
+    view.render options, callback
+  catch error
+    callback error
+
+app.init = () ->
+  @cache = {}
+  @engines = {}
+  @settings = {}
+  @defaultConfiguration()
+  return
+
+app.defaultConfiguration = () ->
+  env = process.env.NODE_ENV || 'development'
+
+  @enable 'x-powered-by'
+  @set 'etag', 'weak'
+  @set 'env', env
+  @set 'query parser', 'extended'
+  @set 'subdomain offset', 2
+  @set 'trust proxy', false
+
+  Object.defineProperty @settings, trustProxyDefaultSymbol, {
+    configurable: true
+    value: true
+  }
+
+  debug 'booting in %s mode', env
+
+  @on 'mount', (parent) ->
+    if @settings[trustProxyDefaultSymbol] == true && typeof parent.settings['trust proxy fn']=='function'
+      delete @settings['trust proxy']
+      delete @settings['trust proxy fn']
+
+    @request.__proto__ = parent.request
+    @response.__proto__ = parent.response
+    @engines.__proto__ = parent.engines
+    @settings.__proto__ = parent.settings
+
+  @locals = Object.create null
+
+  @mounpath = '/'
+
+  @locals.settings = @settings
+
+  @set 'view', View
+  @set 'views', resolve('views')
+  @set 'jsonp callback name', 'callback'
+
+  Object.defineProperty @, 'router', {
+    get: () ->
+      throw new Error '\'app.router\' is deprecated!\nPlease see the 3.x to 4.x migration guide for details on how to update your app.'
+  }
+  return
+
+app.lazyrouter = () ->
+  unless @router
+    @_router = new Router {
+      caseSensitive: @enabled 'case sensitive routing'
+      strict: @enabled 'strict routing'
+    }
+    @_router.use query(@get('query parser fn'))
+    @_router.use middleware.init(@)
+  return
+
+app.handle = (req, res, callback) ->
+  router = @_router
+  done = callback || finalhandler(req, res {
+    env: @get 'env'
+    onerror: logerror.bind @
+    })
+
+  unless router
+    debug 'no routes defined on app'
+    done()
+    return
+
+  router.handle req, res, done
+  return
+
+app.use = (fn) ->
+  offset = 0
+  path = '/'
+
+  if typeof fn != 'function'
+    arg = fn
+
+    while Array.isArray arg && arg.length != 0
+      arg = arg[0]
+
+    if typeof arg != 'function'
+      offset = 1
+      path = fn
+
+  fns = flatten slice.call(arguments, offset)
+
+  if fns.length == 0
+    throw new TypeError 'app.use() requires middleware functions'
+
+  @lazyrouter()
+  router = @_router
+
+  fns.forEach (fn)->
+    if !fn || !fn.handle || !fn.set
+      return router.use path, fn
+    debug '.use app under %s', path
+    fn.mountpath = path
+    fn.parent = @
+
+    router.use path, (req, res, next) ->
+      orig = req.app
+      fn.handle req, res, (err) ->
+        req.__proto__ = orig.request
+        res.__proto__ = orig.response
+        next err
+    fn.emit 'mount',@
+  ,@
+  return @
+
+app.route = (path) ->
+  @lazyrouter()
+  return @_router.route path
+
+app.engine = (ext, fn) ->
+  if typeof fn != 'function'
+    throw new Error 'callback function required'
+  extension =
+    if ext[0] != ''
+    then '.'+ext
+    else ext
+  @engines[extension] = fn
+  return @
+
+app.param = (name, fn) ->
+  @lazyrouter()
+
+  if Array.isArray name
+    for nm in name
+      @param nm, fn
+  return @
+
+app.set = (setting, val) ->
+  if arguments.length == 1
+    return @settings[setting]
+  debug 'set "%s" to %o', setting, val
+
+  @settings[setting] = val
+
+  switch setting
+    when 'etag'
+      @set 'etag fn', compileETag(val)
+      break
+    when 'query parser'
+      @set 'query parser fn', compileQueryParser(val)
+      break
+    when 'trust proxy'
+      @set 'trust proxy fn', compileTrust(val)
+      Object.defineProperty @settings, trustProxyDefaultSymbol, {
+        configurable: true
+        value: false
+        }
+      break
+  return @
+
+app.path = () ->
+  return if @parent then @parent.path() + @mountpath else ''
+
+app.enabled = (setting) ->
+  return Boolean @set(setting)
+
+app.disabled = (setting) ->
+  return !@set setting
+
+app.enable = (setting) ->
+  return @set setting, true
+
+app.disable = (setting) ->
+  return @set setting,false
+
+methods.forEach () ->
+  app[method] = (path) ->
+    if method == 'get' && arguments.length == 1
+      return @get path
+
+    @lazyrouter()
+
+    route = @_router.route path
+    route[method].apply route, slice(arguments, 1)
+    return @
+
+app.all = (path) ->
+  @lazyrouter()
+  route = @_router.route path
+  args = slice.call arguments,1
+
+  for method in methods
+    route[method].apply route, args
+
+  return @
+
+app.del = deprecate.function app.delete, 'app.del: Use app.delete instead'
+
+app.render = (name, options, callback) ->
+  cache = @cache
+  done = callback
+  engines = @engines
+  opts = options
+  renderOptions = {}
+
+  if typeof options == 'function'
+    done = options
+    opts = {}
+
+  merge renderOptions, @locals
+
+  if opts._locals
+    merge renderOptions, opts._locals
+
+  merge renderOptions, opts
+
+  if renderOptions.cache == null
+    renderOptions.cache = @enabled 'view cache'
+
+  if renderOptions.cache
+    view = cache[name]
+
+  unless view
+    View  = @get 'view'
+    view = new View name, {
+      defaultEngine: @get 'view engine'
+      root: @get 'views'
+      engines: engines
+    }
+
+    unless view.path
+      dirs =
+      if Array.isArray view.root && view.root.length >1
+      then 'directories"'+view.root.slice(0,-1).join('","')+'"or"'+view.root[view.root.length-1]+'"'
+      else 'directory"'+view.root + '"'
+      err = new Error 'Failed to lookup view "' + name + '" in views ' + dirs
+      err.view = view
+      return done err
+
+    if renderOptions.cache
+      cache[name] = view
+
+  tryRender view, renderOptions, done
+
+app.listen = () ->
+  server = http.createServer @
+  return server.listen.apply server, arguments
---
+++ CompiledJs/application.js
@@ -0,0 +1,317 @@
+(function() {
+  var Router, View, app, compileETag, compileQueryParser, compileTrust, debug, deprecate, exports, finalhandler, flatten, http, logerror, merge, methods, middleware, query, resolve, slice, trustProxyDefaultSymbol, tryRender;
+
+  finalhandler = require('finalhandler');
+
+  Router = require('./router');
+
+  methods = require('methods');
+
+  middleware = require('./middleware/init');
+
+  query = require('./middleware/query');
+
+  debug = require('debug')('express: application');
+
+  View = require('./view');
+
+  http = require('http');
+
+  compileETag = require('./utils').compileETag;
+
+  compileQueryParser = require('./utils').compileQueryParser;
+
+  compileTrust = require('./utils').compileTrust;
+
+  deprecate = require('depd')('express');
+
+  flatten = require('array-flatten');
+
+  merge = require('utils-merge');
+
+  resolve = require('path').resolve;
+
+  slice = Array.prototype.slice;
+
+  app = exports = module.exports = {};
+
+  trustProxyDefaultSymbol = '@@symbol:trust_proxy_default';
+
+  logerror = function(err) {
+    if (this.get('env') !== 'test') {
+      console.error(err.stack || err.toString());
+    }
+  };
+
+  tryRender = function(view, options, callback) {
+    var error;
+    try {
+      return view.render(options, callback);
+    } catch (_error) {
+      error = _error;
+      return callback(error);
+    }
+  };
+
+  app.init = function() {
+    this.cache = {};
+    this.engines = {};
+    this.settings = {};
+    this.defaultConfiguration();
+  };
+
+  app.defaultConfiguration = function() {
+    var env;
+    env = process.env.NODE_ENV || 'development';
+    this.enable('x-powered-by');
+    this.set('etag', 'weak');
+    this.set('env', env);
+    this.set('query parser', 'extended');
+    this.set('subdomain offset', 2);
+    this.set('trust proxy', false);
+    Object.defineProperty(this.settings, trustProxyDefaultSymbol, {
+      configurable: true,
+      value: true
+    });
+    debug('booting in %s mode', env);
+    this.on('mount', function(parent) {
+      if (this.settings[trustProxyDefaultSymbol] === true && typeof parent.settings['trust proxy fn'] === 'function') {
+        delete this.settings['trust proxy'];
+        delete this.settings['trust proxy fn'];
+      }
+      this.request.__proto__ = parent.request;
+      this.response.__proto__ = parent.response;
+      this.engines.__proto__ = parent.engines;
+      return this.settings.__proto__ = parent.settings;
+    });
+    this.locals = Object.create(null);
+    this.mounpath = '/';
+    this.locals.settings = this.settings;
+    this.set('view', View);
+    this.set('views', resolve('views'));
+    this.set('jsonp callback name', 'callback');
+    Object.defineProperty(this, 'router', {
+      get: function() {
+        throw new Error('\'app.router\' is deprecated!\nPlease see the 3.x to 4.x migration guide for details on how to update your app.');
+      }
+    });
+  };
+
+  app.lazyrouter = function() {
+    if (!this.router) {
+      this._router = new Router({
+        caseSensitive: this.enabled('case sensitive routing'),
+        strict: this.enabled('strict routing')
+      });
+      this._router.use(query(this.get('query parser fn')));
+      this._router.use(middleware.init(this));
+    }
+  };
+
+  app.handle = function(req, res, callback) {
+    var done, router;
+    router = this._router;
+    done = callback || finalhandler(req, res({
+      env: this.get('env'),
+      onerror: logerror.bind(this)
+    }));
+    if (!router) {
+      debug('no routes defined on app');
+      done();
+      return;
+    }
+    router.handle(req, res, done);
+  };
+
+  app.use = function(fn) {
+    var arg, fns, offset, path, router;
+    offset = 0;
+    path = '/';
+    if (typeof fn !== 'function') {
+      arg = fn;
+      while (Array.isArray(arg && arg.length !== 0)) {
+        arg = arg[0];
+      }
+      if (typeof arg !== 'function') {
+        offset = 1;
+        path = fn;
+      }
+    }
+    fns = flatten(slice.call(arguments, offset));
+    if (fns.length === 0) {
+      throw new TypeError('app.use() requires middleware functions');
+    }
+    this.lazyrouter();
+    router = this._router;
+    fns.forEach(function(fn) {
+      if (!fn || !fn.handle || !fn.set) {
+        return router.use(path, fn);
+      }
+      debug('.use app under %s', path);
+      fn.mountpath = path;
+      fn.parent = this;
+      router.use(path, function(req, res, next) {
+        var orig;
+        orig = req.app;
+        return fn.handle(req, res, function(err) {
+          req.__proto__ = orig.request;
+          res.__proto__ = orig.response;
+          return next(err);
+        });
+      });
+      return fn.emit('mount', this);
+    }, this);
+    return this;
+  };
+
+  app.route = function(path) {
+    this.lazyrouter();
+    return this._router.route(path);
+  };
+
+  app.engine = function(ext, fn) {
+    var extension;
+    if (typeof fn !== 'function') {
+      throw new Error('callback function required');
+    }
+    extension = ext[0] !== '' ? '.' + ext : ext;
+    this.engines[extension] = fn;
+    return this;
+  };
+
+  app.param = function(name, fn) {
+    var i, len, nm;
+    this.lazyrouter();
+    if (Array.isArray(name)) {
+      for (i = 0, len = name.length; i < len; i++) {
+        nm = name[i];
+        this.param(nm, fn);
+      }
+    }
+    return this;
+  };
+
+  app.set = function(setting, val) {
+    if (arguments.length === 1) {
+      return this.settings[setting];
+    }
+    debug('set "%s" to %o', setting, val);
+    this.settings[setting] = val;
+    switch (setting) {
+      case 'etag':
+        this.set('etag fn', compileETag(val));
+        break;
+      case 'query parser':
+        this.set('query parser fn', compileQueryParser(val));
+        break;
+      case 'trust proxy':
+        this.set('trust proxy fn', compileTrust(val));
+        Object.defineProperty(this.settings, trustProxyDefaultSymbol, {
+          configurable: true,
+          value: false
+        });
+        break;
+    }
+    return this;
+  };
+
+  app.path = function() {
+    if (this.parent) {
+      return this.parent.path() + this.mountpath;
+    } else {
+      return '';
+    }
+  };
+
+  app.enabled = function(setting) {
+    return Boolean(this.set(setting));
+  };
+
+  app.disabled = function(setting) {
+    return !this.set(setting);
+  };
+
+  app.enable = function(setting) {
+    return this.set(setting, true);
+  };
+
+  app.disable = function(setting) {
+    return this.set(setting, false);
+  };
+
+  methods.forEach(function() {
+    return app[method] = function(path) {
+      var route;
+      if (method === 'get' && arguments.length === 1) {
+        return this.get(path);
+      }
+      this.lazyrouter();
+      route = this._router.route(path);
+      route[method].apply(route, slice(arguments, 1));
+      return this;
+    };
+  });
+
+  app.all = function(path) {
+    var args, i, len, method, route;
+    this.lazyrouter();
+    route = this._router.route(path);
+    args = slice.call(arguments, 1);
+    for (i = 0, len = methods.length; i < len; i++) {
+      method = methods[i];
+      route[method].apply(route, args);
+    }
+    return this;
+  };
+
+  app.del = deprecate["function"](app["delete"], 'app.del: Use app.delete instead');
+
+  app.render = function(name, options, callback) {
+    var cache, dirs, done, engines, err, opts, renderOptions, view;
+    cache = this.cache;
+    done = callback;
+    engines = this.engines;
+    opts = options;
+    renderOptions = {};
+    if (typeof options === 'function') {
+      done = options;
+      opts = {};
+    }
+    merge(renderOptions, this.locals);
+    if (opts._locals) {
+      merge(renderOptions, opts._locals);
+    }
+    merge(renderOptions, opts);
+    if (renderOptions.cache === null) {
+      renderOptions.cache = this.enabled('view cache');
+    }
+    if (renderOptions.cache) {
+      view = cache[name];
+    }
+    if (!view) {
+      View = this.get('view');
+      view = new View(name, {
+        defaultEngine: this.get('view engine'),
+        root: this.get('views'),
+        engines: engines
+      });
+      if (!view.path) {
+        dirs = Array.isArray(view.root && view.root.length > 1) ? 'directories"' + view.root.slice(0, -1).join('","') + '"or"' + view.root[view.root.length - 1] + '"' : 'directory"' + view.root + '"';
+        err = new Error('Failed to lookup view "' + name + '" in views ' + dirs);
+        err.view = view;
+        return done(err);
+      }
+      if (renderOptions.cache) {
+        cache[name] = view;
+      }
+    }
+    return tryRender(view, renderOptions, done);
+  };
+
+  app.listen = function() {
+    var server;
+    server = http.createServer(this);
+    return server.listen.apply(server, arguments);
+  };
+
+}).call(this);
